3c4dd3185e2e28c9e6a9642d5b7f1220a86df07b
Raul <r.far@cleverpy.com>
2022-12-21 12:19:49 +0100

    upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const { createServer }= require('@lhci/server');
+
+console.log("starting server")
+
+createServer({
+port:9001,
+
+storage: { 
+storageMethod: 'sql',
+sqlDialect: 'mysql',
+sqlConnectionSsl: false,
+sqlConnectionUrl: 'mysql://root@127.0.01:3306/lighthouse'
+}
+
+}).then(({ port }) => {
+    console.log("port: ", port)
+}).catch(( error )  =>{
+    console.log("error:", error)
+})
+
