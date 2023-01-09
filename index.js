60e52c8489db918cde710e122e1d1d5363a45aa0
raul far <raulfarllobell@gmail.com>
2023-01-09 10:27:28 +0100

    changes

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const { createServer }= require('@lhci/server');
-
-console.log("starting  t4 server")
+const { createServer } = require('@lhci/server');
 
 createServer({
-port:9001,
+    port: 9001,
 
-storage: { 
-storageMethod: 'sql',
-sqlDialect: 'mysql',
-sqlConnectionSsl: false,
-sqlConnectionUrl: 'mysql://root@127.0.01:3306/lighthouse'
-}
-
+    storage: {
+        storageMethod: 'sql',
+        sqlDialect: 'mysql',
+        sqlConnectionSsl: false,
+        sqlConnectionUrl: 'mysql://root@127.0.01:3306/lighthouse'
+    }
 }).then(({ port }) => {
     console.log("port: ", port)
-}).catch(( error )  =>{
+}).catch((error) => {
     console.log("error:", error)
 })
 
